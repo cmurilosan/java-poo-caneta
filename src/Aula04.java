eb01aa547f1b9fcc933154f78b2caa1d21e6024d
Cassio Murilo <cmurilo.san@gmail.com>
2020-03-12 16:24:07 -0300

    POO aula 04

---
+++ src/Aula04.java
@@ -0,0 +1,15 @@
+
+public class Aula04 {
+	
+	public static void main(String[] args) {
+		
+		CanetaAula04 c1 = new CanetaAula04("NIC", "Amarela", 0.4f);
+		c1.Status();
+//		c1.setPonta(0.5f);
+//		c1.Status();
+//		
+//		System.out.println("Tenho uma caneta " + c1.getModelo() + " de ponta " + c1.getPonta());
+		
+		
+	}
+}
---
+++ src/TestaCanetaAula03.java
@@ -0,0 +1,18 @@
+
+public class TestaCanetaAula03 {
+	
+	public static void main(String[] args) {
+		
+		CanetaAula03 c1 = new CanetaAula03();
+
+		c1.modelo = "BIC Cristal";
+		c1.cor = "Azul";
+//		c1.ponta = 0.5f;
+		c1.carga = 80;
+//		c1.tampada = true;
+		c1.destampar();
+		c1.status();		
+		
+	}
+
+}
