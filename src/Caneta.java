7606a8776fc2a5d3bb3a5a7c436aab279fef9024
Cassio Murilo <cmurilo.san@gmail.com>
2020-03-12 13:31:18 -0300

    POO aula 03

---
+++ src/Caneta.java
@@ -0,0 +1,36 @@
+
+public class Caneta {
+
+//	AULA 03
+//	
+//	public String modelo;
+//	public String cor;
+//	private float ponta;
+//	protected int carga;
+//	private boolean tampada;
+//	
+//	public void status() {
+//		System.out.println("Modelo: " + this.modelo);
+//		System.out.println("Uma caneta " + this.cor);
+//		System.out.println("Ponta: " + this.ponta);
+//		System.out.println("Carga: " + this.carga);
+//		System.out.println("Está tampada? " + this.tampada);
+//	}
+//	
+//	public void rabiscar() {
+//		if(tampada == true) {
+//			System.out.println("Não pode rabiscar. A caneta está tampada!");
+//		} else {
+//			System.out.println("Pode rabiscar!!!");
+//		}	
+//	}
+//
+//	public void tampar() {
+//		this.tampada = true;	
+//	}
+//	
+//	public void destampar() {
+//		this.tampada = false;
+//		
+//	}
+}
